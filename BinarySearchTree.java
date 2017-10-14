eb1a83b3c2b5ab7d8f78c0fe3dab3843816ecd9a
Ruichen Tang <trc2000797@foxmail.com>
2017-10-13 20:14:45 -0400

    Add files via upload
    BST into real use

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class BinarySearchTree {
 
     public static Node root;
@@ -18,9 +21,14 @@ public class BinarySearchTree {
         }
     }
 
-    public void insert(int value) {
+    public boolean isEmpty() {
+        if(root == null) return true;
+        else return false;
+    }
+
+    public void add(int value) {
         //if the tree is empty
-        if (root == null) {
+        if (isEmpty()) {
             root = new Node(value);
             return;
         }
@@ -36,18 +44,18 @@ public class BinarySearchTree {
                 if (current == null) {
                     parent.left = new Node(value);
                     return;
-                } else {
-                    current = current.right;
-                    if (current == null) {
-                        parent.right = new Node(value);
-                        return;
-                    }
+                }
+            } else {
+                current = current.right;
+                if (current == null) {
+                    parent.right = new Node(value);
+                    return;
                 }
             }
         }
     }
 
-    public boolean Search(int value) {
+    public boolean contains(int value) {
         Node current = root;
         while (current != null) {
             if(current.data == value)
@@ -57,6 +65,76 @@ public class BinarySearchTree {
             else current = current.right;
         }
         return false;
+    }
+
+    public boolean remove(int value) {
+        Node parent = root;
+        Node current = root;
+        boolean isLeftChild = false;
+
+        while (current.data != value) {
+            parent = current;
+            if (current.data > value) {
+                isLeftChild = true;
+                current = current.left;
+            } else {
+                isLeftChild = false;
+                current = current.right;
+            }
+            if (current == null) {
+                return false;
+            }
+        }
+        if (current.left == null && current.right == null) {
+            if (current == root) {
+                root = null;
+            }
+            if (isLeftChild == true) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        }
+        else if (current.right == null) {
+            if(current == root) {
+                root = current.left;
+            } else if(isLeftChild) {
+                parent.left = current.left;
+            } else { parent.right = current.left; }
+        }
+        else if(current.left == null) {
+            if(current == root) {
+                root = current.right;
+            } else if(isLeftChild){
+                parent.left = current.right;
+            } else { parent.right = current.right;}
+        } else if(current.left != null && current.right != null) {
+            Node successor = getSuccessor(current);
+            if(current == root) {
+                root = successor;
+            } else if(isLeftChild) {
+                parent.left = successor;
+            } else { parent.right = successor; }
+        successor.left = current.left;
+        }
+        return true;
+    }
+
+    public Node getSuccessor(Node deleteNode) {
+        Node successor = null;
+        Node successorParent = null;
+        Node current = deleteNode.right;
+
+        while(current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.left;
+        }
+        if(successor != deleteNode.right) {
+            successorParent.left = successor.right;
+            successor.right = deleteNode.right;
+        }
+        return successor;
     }
 
     public void display(Node root) {
@@ -71,10 +149,61 @@ public class BinarySearchTree {
 
     public static void main(String[] args) {
         //try here
-        BinarySearchTree b = new BinarySearchTree();
-        b.insert(3);b.insert(8);
-        b.insert(1);b.insert(6);
-        System.out.print("Original Tree : ");
-        b.display(b.root);
+
+
+        //The name of the file to open
+        String fileName = "src/infile.dat";
+        String str = null;
+        //This will reference one line at a time
+        String line = null;
+
+        try{
+            FileReader fileReader = new FileReader(fileName);
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println("Sorted Set A Contains " + line);
+                //Get the String array divided into mutiple numbers
+                String[] numbers = line.split(",");
+                //Store int
+                int[] intNumbers = new int[numbers.length];
+                for (int i = 0; i < numbers.length; i++) {
+                    intNumbers[i] = Integer.parseInt(numbers[i]);
+                }
+
+                //Init the tree here
+                BinarySearchTree b = new BinarySearchTree();
+
+                for(int j = 0; j < intNumbers.length; j++) {
+                    b.add(intNumbers[j]);
+                }
+
+//                b.display(b.root);
+                System.out.println();
+                System.out.print("User Input = ");
+
+                //grab an input from user
+                int in;
+                Scanner keyboard = new Scanner(System.in);
+                in = keyboard.nextInt();
+                if(b.contains(in)) {
+                    System.out.println("Yes");
+                } else {
+                    System.out.println("No");
+                }
+
+            }
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName);
+        }
+        catch(IOException ex) {
+            System.out.println("Error reading file'" + fileName + "'");
+            ex.printStackTrace();
+        }
+
     }
 }
+
